37b79778a01c74f74c26e35a221cf72db732543f
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2023-12-20 13:04:10 +0100

    run project in local

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {I18nextProvider} from "react-i18next";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 
-import TagManager from 'react-gtm-module'
+import TagManager from "react-gtm-module";
 
-import './i18n';
-
+import "./i18n";
 
 const tagManagerArgs = {
-  gtmId: 'GTM-TFQTQZ3'
-}
-
-
+  gtmId: "GTM-TFQTQZ3",
+};
 
 ReactDOM.render(
-  <React.Fragment>
-    <I18nextProvider i18n={i18next}>
-      <App/>
-    </I18nextProvider>
-  </React.Fragment>,
-  document.getElementById('root')
+  <I18nextProvider i18n={i18next}>
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  </I18nextProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
