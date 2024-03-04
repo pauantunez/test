07cd973fbb766cd16093627d21f11c8c75745aea
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-03-04 15:08:46 +0100

    corrrections step

--- src/components/tabs/building/step1.jsx
+++ src/components/tabs/building/step1.jsx
@@ -1,64 +1,39 @@
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router-dom";
-import AppContext from '../../../AppContext'
-import {
-  Button,
-} from 'reactstrap';
-import axios from 'axios';
-import BuildingSize from './components/buildingSize'
+import AppContext from "../../../AppContext";
+import BuildingSize from "./components/buildingSize";
 
-import { withTranslation } from 'react-i18next';
-
-
-
-var entryParam;
-var foundTheme;
-var btnFont;
-var fontHeadline;
-var fontRegular;
-var btnColor;
+import { withTranslation } from "react-i18next";
 
 class Tab1Step1 extends React.Component {
-  
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            overlayToggle: false,
-            imprint: [],
-            theme: props.theme
-        }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      overlayToggle: false,
+      imprint: [],
+      theme: props.theme,
+    };
+  }
+
+  static contextType = AppContext;
+
+  componentDidMount() {}
+
+  async toggleModal() {
+    if (this.state.overlayToggle) {
+      this.setState({ overlayToggle: false });
+    } else {
+      this.setState({ overlayToggle: true });
     }
+  }
 
-    static contextType = AppContext
-
-    componentDidMount() {
-        
-    }
-
-    async toggleModal() {
-
-      if(this.state.overlayToggle) {
-          this.setState({overlayToggle: false})
-      } else {
-          this.setState({overlayToggle: true})
-      }
-
-    }
-
-    render() {
-
-      const { t } = this.props;
-      const { overlayToggle } = this.state;
-
-          return  ( 
-          <div>
-
-            <BuildingSize />
-
-          </div>
-          )
-
+  render() {
+    return (
+      <div>
+        <BuildingSize />
+      </div>
+    );
   }
 }
 
