280a5ab0050dbc8a8eaaab3e3792d5d457d5ede8
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-03-11 16:59:33 +0100

    remove unused vars step3

--- src/components/tabs/result/step3.jsx
+++ src/components/tabs/result/step3.jsx
@@ -1,140 +1,90 @@
-import React from 'react';
-import Box from '@mui/material/Box';
+import React from "react";
 import { withRouter } from "react-router-dom";
-import AppContext from '../../../AppContext'
-import {
-  Button,
-} from 'reactstrap';
-import axios from 'axios';
-import Additional from './components/additional';
-import BreakEven from './components/breakEven';
-import Cost from './components/cost';
-import OffGrid from './components/offGrid';
-import ElectricityUse from './components/electricityUse';
-import HouseholdUse from './components/householdUse';
+import AppContext from "../../../AppContext";
+import Additional from "./components/additional";
 
-import {Typography} from '@mui/material';
-import { withTranslation } from 'react-i18next';
-import TextField from '@mui/material/TextField';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Slider from '@mui/material/Slider';
-import NativeSelect from '@mui/material/NativeSelect';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
-import Switch from '@mui/material/Switch';
+import { withTranslation } from "react-i18next";
 
-import {ReactComponent as BoschLogo} from '../../../assets/img/bosch.svg';
-
-import { jsPDF } from "jspdf";
 import "jspdf-autotable";
-import html2canvas from 'html2canvas';
-
-var entryParam;
-var foundTheme;
-var btnFont;
-var fontHeadline;
-var fontRegular;
-var btnColor;
 
 class ResultStep3 extends React.Component {
-  
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            overlayToggle: false,
-            imprint: [],
-            theme: props.theme
-        }
+  constructor(props) {
+    super(props);
 
-        
-    }
-
-    static contextType = AppContext
-
-    componentDidMount() {
-      
-    }
-
-    onInputchange(event) {
-      this.setState({
-        [event.target.name]: event.target.value
-      });
-    }
-
-    inputPower_kW_PV_MFH = (event) => { 
-      const { overlayToggle, Eta_sh_gas_EDWW_MFH_Brine, setGasBrine, Power_kW_PV_MFH, setPower_kW_PV_MFH } = this.context;
-
-      setPower_kW_PV_MFH(event.target.value) 
+    this.state = {
+      overlayToggle: false,
+      imprint: [],
+      theme: props.theme,
     };
-
-    inputBuildingEnegeryStandard = (event) => { 
-      const { BuildingEnegeryStandard, setBuildingEnegeryStandard} = this.context;
-      setBuildingEnegeryStandard(event.target.value) 
-    };
-
-    inputBuildingSize = (event) => { 
-      const { BuildingSize, setBuildingSize} = this.context;
-      setBuildingSize(event.target.value) 
-    };
-
-    inputGasOilSwitch = (event) => { 
-      const { GasOilSwitch, setGasOilSwitch} = this.context;
-      setGasOilSwitch(event.target.checked) 
-    };
-
-    investmentCost = () => {
-      const { pvOutputkWh, homeStorageSize, PVcostLookupTable, investmentCostEUR, StorageCostLookupTable, addHeatpumpPVems } = this.context;
-
-      let PVcostInTable = PVcostLookupTable.find((o) => o.pv === pvOutputkWh);
-      let StorageCostInTable = StorageCostLookupTable.find((o) => o.storage === homeStorageSize);
-  
-      let investmentCostResult = Math.abs(PVcostInTable.cost);
-  
-      if (investmentCostEUR > 0) {
-        investmentCostResult = parseInt(investmentCostEUR) * -1;
-      }
-  
-      return Math.abs(investmentCostResult);
-    };
-
-    async toggleModal() {
-
-      if(this.state.overlayToggle) {
-          this.setState({overlayToggle: false})
-      } else {
-          this.setState({overlayToggle: true})
-      }
-
-    }
-
-    async changeVolume(e,value) {
-      const { EnergyUse, setEnergyUse} = this.context;
-      setEnergyUse(value)
-      console.log(value)
-
   }
 
-    render() {
+  static contextType = AppContext;
 
-      const { t } = this.props;
-      const { overlayToggle, Eta_sh_gas_EDWW_MFH_Brine, setGasBrine, Power_kW_PV_MFH, setPower_kW_PV_MFH, BuildingEnegeryStandard, SetBuildingEnegeryStandard, EnergyUse, setEnergyUse, BuildingSize, GasOilSwitch } = this.context;
+  componentDidMount() {}
 
-      sessionStorage.setItem("InvestmentCostEUR", this.investmentCost().toLocaleString("DE-de"))
+  onInputchange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
 
-          return  ( 
-          <div style={{marginLeft: '3%', marginRight: '3%'}}>
+  inputPower_kW_PV_MFH = (event) => {
+    const { setPower_kW_PV_MFH } = this.context;
 
-            <Additional/>
+    setPower_kW_PV_MFH(event.target.value);
+  };
 
-          </div>
-          )
+  inputBuildingEnegeryStandard = (event) => {
+    const { setBuildingEnegeryStandard } = this.context;
+    setBuildingEnegeryStandard(event.target.value);
+  };
 
+  inputBuildingSize = (event) => {
+    const { setBuildingSize } = this.context;
+    setBuildingSize(event.target.value);
+  };
+
+  inputGasOilSwitch = (event) => {
+    const { setGasOilSwitch } = this.context;
+    setGasOilSwitch(event.target.checked);
+  };
+
+  investmentCost = () => {
+    const { pvOutputkWh, PVcostLookupTable, investmentCostEUR } = this.context;
+
+    let PVcostInTable = PVcostLookupTable.find((o) => o.pv === pvOutputkWh);
+
+    let investmentCostResult = Math.abs(PVcostInTable.cost);
+
+    if (investmentCostEUR > 0) {
+      investmentCostResult = parseInt(investmentCostEUR) * -1;
+    }
+
+    return Math.abs(investmentCostResult);
+  };
+
+  async toggleModal() {
+    if (this.state.overlayToggle) {
+      this.setState({ overlayToggle: false });
+    } else {
+      this.setState({ overlayToggle: true });
+    }
+  }
+
+  async changeVolume(e, value) {
+    const { setEnergyUse } = this.context;
+    setEnergyUse(value);
+    console.log(value);
+  }
+
+  render() {
+    sessionStorage.setItem("InvestmentCostEUR", this.investmentCost().toLocaleString("DE-de"));
+
+    return (
+      <div style={{ marginLeft: "3%", marginRight: "3%" }}>
+        <Additional />
+      </div>
+    );
   }
 }
 
