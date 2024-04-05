70491bfed71ae560a862206d249e90c7a7a7e43a
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-04-05 15:36:24 +0200

    remove unused vars

--- src/components/tabs/facilities/step1.jsx
+++ src/components/tabs/facilities/step1.jsx
@@ -1,70 +1,45 @@
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router-dom";
-import AppContext from '../../../AppContext'
-import {
-  Button,
-} from 'reactstrap';
-import axios from 'axios';
+import AppContext from "../../../AppContext";
 
-import { withTranslation } from 'react-i18next';
-import TextField from '@mui/material/TextField';
-import Heatpump from './components/heatpump';
-
-
-var entryParam;
-var foundTheme;
-var btnFont;
-var fontHeadline;
-var fontRegular;
-var btnColor;
+import { withTranslation } from "react-i18next";
+import Heatpump from "./components/heatpump";
 
 class FacilitiesStep1 extends React.Component {
-  
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            overlayToggle: false,
-            imprint: [],
-            theme: props.theme
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    static contextType = AppContext
-
-    componentDidMount() {
-        
-    }
-
-    inputTCO_thermal_EUR_a = (event) => { 
-      const { overlayToggle, Eta_sh_gas_EDWW_MFH_Brine, setGasBrine, Power_kW_PV_MFH, setPower_kW_PV_MFH, TCO_thermal_EUR_a, setTCO_thermal_EUR_a } = this.context;
-
-      setTCO_thermal_EUR_a(event.target.value) 
+    this.state = {
+      overlayToggle: false,
+      imprint: [],
+      theme: props.theme,
     };
+  }
 
-    async toggleModal() {
+  static contextType = AppContext;
 
-      if(this.state.overlayToggle) {
-          this.setState({overlayToggle: false})
-      } else {
-          this.setState({overlayToggle: true})
-      }
+  componentDidMount() {}
 
+  inputTCO_thermal_EUR_a = (event) => {
+    const { setTCO_thermal_EUR_a } = this.context;
+
+    setTCO_thermal_EUR_a(event.target.value);
+  };
+
+  async toggleModal() {
+    if (this.state.overlayToggle) {
+      this.setState({ overlayToggle: false });
+    } else {
+      this.setState({ overlayToggle: true });
     }
+  }
 
-    render() {
-
-      const { t } = this.props;
-      const { overlayToggle, Eta_sh_gas_EDWW_MFH_Brine, setGasBrine, Power_kW_PV_MFH, setPower_kW_PV_MFH, TCO_thermal_EUR_a, setTCO_thermal_EUR_a } = this.context;
-
-          return  ( 
-          <div>
-
-            <Heatpump />
-
-          </div>
-          )
-
+  render() {
+    return (
+      <div>
+        <Heatpump />
+      </div>
+    );
   }
 }
 
