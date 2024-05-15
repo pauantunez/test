fae13a4329a84e0dc42b8494f58e4942f1830c77
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-05-15 13:00:33 +0200

    solve error graph pdf

--- src/components/tabs/result/components/costPdf.jsx
+++ src/components/tabs/result/components/costPdf.jsx
@@ -146,7 +146,7 @@ class CostPdf extends React.Component {
 
     // 20 years bar heights
     var twentyYearsHeightMitPv = this.getBarHeights(cost20YearNoPV, cost20yearPV, savingOnlyPV20years);
-    var twentyYearsHeightMitPvAndEms = this.getBarHeights(cost20YearNoPV, cost20yearPV, savingPVandEMS20years);
+    var twentyYearsHeightMitPvAndEms = this.getBarHeights(cost20YearNoPV, cost20yearPVEMS, savingPVandEMS20years);
 
     return (
       <div>
