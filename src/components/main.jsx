ea43903886520539dacfcd767fc9afe8ea4dee61
daniramosnv <daniel.ramos@novaigrup.com>
2024-06-17 15:02:16 +0200

    Test scroll minus header

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -127,8 +127,24 @@ class Main extends React.Component {
 
   scrollToTop() {
     if (this.mainRef.current) {
-      this.mainRef.current.scrollIntoView();
+          
+          if (this.mainRef.current) {       
+            // Access the header height from the parent window
+            const headerElement = window.parent.document.getElementById('header');                 
+            const targetPosition = this.mainRef.current.getBoundingClientRect().top + window.scrollY;
+            if(headerElement){
+              const headerHeight = headerElement.offsetHeight;
+              targetPosition -= headerHeight;
+            }
+
+            window.scrollTo({
+              top: targetPosition,
+              behavior: 'smooth'
+            });
+          }
+
     }
+    
   }
 
   componentDidMount() {
